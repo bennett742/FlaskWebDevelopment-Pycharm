1b55d86dad0d6bfa664de0ff66c9a2393d5eedd8
bennett742 <79178799+bennett742@users.noreply.github.com>
2021-09-10 16:30:39 -0400

    Add files via upload

---
+++ Ex 3-11.py
@@ -0,0 +1,32 @@
+from flask_bootstrap import Bootstrap
+from flask-moment import Moment
+from flask import Flask
+
+app = Flask(__name__)
+
+moment = Moment(app)
+
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' % name
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+
+bootstrap = Bootstrap(app)
